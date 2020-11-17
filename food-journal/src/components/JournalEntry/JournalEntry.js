7725d9f6d62af9ca09e7fc46cd7cdd32bd57579d
Akil Gray <akilgray@Akils-MacBook-Air.local>
2020-11-17 18:32:51 -0500

    journalEntry resize problem fixed

--- food-journal/src/components/JournalEntry/JournalEntry.js
+++ food-journal/src/components/JournalEntry/JournalEntry.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 import { timeFormatConvert } from "../../shared/utility";
 
 const JournalEntry = (props) => {
-  const { serving_qty, food_name, serving_unit } = props.entry;
-  let { consumed_at } = props.entry;
+  const { serving_qty, food_name  } = props.entry;
+  let { consumed_at, serving_unit } = props.entry;
   consumed_at = timeFormatConvert(consumed_at);
-
+  if(serving_unit.length > 5){
+    serving_unit = serving_unit.slice(0,6)
+  } 
   let description = `${serving_qty} ${serving_unit} of ${food_name}`;
   if (food_name === serving_unit) {
     description = `${serving_qty} ${food_name}`;
